4e2e35ff87881cff7fe5b81be042f8ad12500219
guangqianzhang <gqzhang77@163.com>
2022-07-26 21:35:31 +0800

    julei by c++

---
+++ cmake-build-debug/CMakeFiles/test5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test5.dir/main.cpp.o"
+  "CMakeFiles/test5.dir/src/ProcessPointClouds.cpp.o"
+  "CMakeFiles/test5.dir/src/render.cpp.o"
+  "test5"
+  "test5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/test52.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test52.dir/main2.cpp.o"
+  "CMakeFiles/test52.dir/src/ProcessPointClouds.cpp.o"
+  "CMakeFiles/test52.dir/src/render.cpp.o"
+  "test52"
+  "test52.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test52.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
